2bab0776173805dbc8321b03f9fa41974fa18bfd
mengao <meng_ao@cdv.com>
2017-12-05 17:46:48 +0800

    完成join测试和创建

--- src/main/scala/JoinTest.scala
+++ src/main/scala/JoinTest.scala
@@ -1,4 +1,6 @@
-import org.apache.spark.sql.SparkSession
+import com.sun.prism.PixelFormat.DataType
+import org.apache.spark.sql.{Row, SparkSession}
+import org.apache.spark.sql.types.{DataTypes, StructField, StructType}
 
 object JoinTest {
 
@@ -17,7 +19,20 @@ object JoinTest {
     import spark.implicits._
     val df = spark.sparkContext.textFile("1.txt").map(_.split(" ")).map(x => Person(x(0), x(1))).toDF()
     val df2 = spark.sparkContext.textFile("2.txt").map(_.split(" ")).map(x => Person(x(0), x(1))).toDF()
-    df.join(df2, "age").show(false)
+//    df.join(df2, "age").show(false)
+
+
+
+    val schemaString ="age name"
+    val fields   = schemaString .split(" ").map(x=>StructField(x,DataTypes.StringType,true))
+    val schema =StructType(fields)
+    val rowRDD = spark.sparkContext.textFile("2.txt").map(_.split(" ")).map(x=>Row(x(0),x(1)))
+    spark.createDataFrame(rowRDD,schema )
+    
+//    val intToString = Map(1->"a1",2->"a2",3->"a3")
+      val frame = Seq(Person("1","a1"),Person("2","a2")).toDF()
+      frame.printSchema()
+      frame.show()
 
 
 
