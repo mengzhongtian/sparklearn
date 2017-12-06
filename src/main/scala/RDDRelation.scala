5c53e32e77867c8fbc97e6677a63fe49a94d6838
mike <mengzhongtian@126.com>
2017-12-06 18:51:38 +0800

    学习

---
+++ src/main/scala/RDDRelation.scala
@@ -0,0 +1,46 @@
+import org.apache.spark.sql.{SaveMode, SparkSession}
+
+object RDDRelation {
+
+  case class Record(key: Int, value: String)
+
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession
+      .builder()
+      .appName("Demo_HotWords")
+      .master("local[2]")
+      .config("spark.sql.crossJoin.enabled", "true")
+      .getOrCreate()
+
+    import spark.implicits._
+
+    /**
+      * 创建DataFrame
+      **/
+
+    val DF = spark.createDataFrame((1 to 100).map(x => Record(x, s"value=$x")))
+    DF.createOrReplaceTempView("record")
+    val sqlDF = spark.sql("select * from record")
+    //    sqlDF.collect().foreach(println)
+//    sqlDF.foreach(row => {
+//      println(row(0), row(1))
+//    })
+
+//    sqlDF.foreach(row=>println(row))
+//    spark.sparkContext.textFile("").foreach(println)
+    val head = spark.sql("select count(*) from record").collect().head.getLong(0)
+
+    val slqDF2 = spark.sql("select key,value from record where key <10")
+//    slqDF2.rdd.map(row=>s"key:${row(0)},value:${row(1)}").collect().foreach(println)
+//    slqDF2.map(row=>s"key:${row(0)},value:${row(1)}").collect().foreach(println)
+
+    DF.where(" key <10").where($"key"<4).orderBy($"key".desc).select("value").show()
+
+    DF.write.mode(SaveMode.Overwrite).parquet("1.parquet")
+    val df3 = spark.read.parquet("1.parquet")
+
+    spark.stop()
+
+  }
+
+}
--- src/main/scala/Test.scala
+++ src/main/scala/Test.scala
@@ -1,6 +1,15 @@
+import org.apache.spark.sql.SparkSession
+
 object Test {
   def main(args: Array[String]): Unit = {
-print(    List(2,3,1,0,5))
+
+    val spark = SparkSession
+      .builder()
+      .appName("Demo_HotWords")
+      .master("local[2]")
+      .config("spark.sql.crossJoin.enabled", "true")
+      .getOrCreate()
+    spark.read.parquet("1.parquet").show()
   }
 
 }
